b07dccd2974e90db3aee218d2d7f8fe834b762be
nut <suppanat.nut@gmail.com>
2024-04-04 01:14:40 +0700

    resolve js functions

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,26 @@
 // Start coding here
+
+//function add
+const x = function (a,b) {return a+b;};
+let add = x(10,5);
+
+
+//function subtract
+const y = function (a,b) {return a-b;};
+let subtract = y(10,5);
+
+
+//function multiply
+const z = function (a,b) {return a*b;};
+let multiply = z(10,5);
+
+
+//function divide
+const w = function (a,b) {return a/b;};
+let divide = w(10,5);
+
+
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
